03789373212db7a7d5842578ed91d8e12f0e6422
Dheeraj <dheeraj_sisodia@ymail.com>
2018-10-28 16:40:03 +0530

    proxy changes

--- demo-user-service/src/main/java/com/microservice/demouserservice/UserOrderProxy.java
+++ demo-user-service/src/main/java/com/microservice/demouserservice/UserOrderProxy.java
@@ -9,9 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 //@FeignClient(name="currency-exchange-service", url="localhost:8000")
-@FeignClient(name="order-service",url="localhost:8200")
+@FeignClient(name="order-service")
 //@FeignClient(name="netflix-zuul-api-gateway-server")
-//@RibbonClient(name = "order-service")
+@RibbonClient(name = "order-service")
 public interface UserOrderProxy {
 	// @GetMapping("/currency-exchange/from/{from}/to/{to}")
 	@GetMapping("/order/userId/{userId}")
