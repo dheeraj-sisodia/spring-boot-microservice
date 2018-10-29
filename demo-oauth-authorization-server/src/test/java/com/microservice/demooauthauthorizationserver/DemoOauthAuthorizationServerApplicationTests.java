6cd7e7e33704b28c144fddef52a4b1574ee69126
Dheeraj <dheeraj_sisodia@ymail.com>
2018-10-29 20:13:24 +0530

    Token server added

---
+++ demo-oauth-authorization-server/src/test/java/com/microservice/demooauthauthorizationserver/DemoOauthAuthorizationServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.microservice.demooauthauthorizationserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoOauthAuthorizationServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
